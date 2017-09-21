da5f63434c218c9cff33980060e1b138e7ac43e0
Adam Hynson <hynsondevelops@gmail.com>
2017-09-21 17:01:08 -0400

    Random Reviews updated

--- script.js
+++ script.js
@@ -1,4 +1,54 @@
-var sampleReview = {title: 'Level 1', text: 'Micheal is progressing so well in his speaking! He is listening, paying attention, and repeating very well! Michael can circle first letters of words to help with reading. He understood all of the directions and circled all of the answers correctly too! Wow. Keep working hard. See you next time from Teacher Deborah'};
+//name -> info in name field
+//He -> He or She depending on gender field
+//Replace what is in the single quotes with the content for previp trial
+var contentPreVIPTrial = 'It was so nice to meet name today! He is a hard working student. He speaks very well! He could repeat all of the words and letters. He is young and learning this type of classroom. I am impressed at how quickly name learns! He said: "happy, star, bear, bubbles, monkey, yes, ok,go". The teachers here are all kind and friendly. He will feel comfortable to learn English.  Excellent 5 star job! I think with practice in this type of setting, name will rapidly advance in the program. I think starting at a young age gives name a big advantage. name is an excellent candidate for VIPKID.'
+
+//Replace what is in the single quotes with the content for nonreader trial
+var contentNonreaderTrial = 'It was so nice to meet name today! He is a hard working student. He speaks very well! He could repeat all of the words and letters. He is young and learning this type of classroom. I am impressed at how quickly name learns! He said: "happy, star, bear, bubbles, monkey, yes, ok,go". The teachers here are all kind and friendly. He will feel comfortable to learn English. Excellent 5 star job! I think with practice in this type of setting, he will rapidly advance in the program. I think starting at a young age is a big advantage. name is an excellent candidate for VIPKID. Excellent 5 star job!'
+
+//Replace what is in the single quotes with the content for reader trial
+var contentReaderTrial = 'It was so nice to meet name today! He is a hard working student. name speaks very well! He could repeat all of the words and sentences. name did a good job with reading. Practice speaking with a native speaker will help name make fast progress with pronunciation. The teachers here are all kind and friendly. He will feel comfortable to expand on already good English. Excellent 5 star job! I think with practice in this type of setting he will rapidly advance in the program. name is an excellent candidate for VIPKID. Excellent 5 star job!'
+
+//Replace what is in the single quotes with the content for MC previp
+var contentMCPreVIP = 'It is nice to meet adorable name. He participates fully in class. He can practice putting "a" in front of nouns. "I see a cat". name knows all of the letters and can match big to small. He can put together sentences with the help of the teacher. "I draw a line. I love Dino". name sang three songs today!! Please review the notes on the text box in the lesson video for specific words and grammar to review. Great 5 happy star job today.'
+
+//Replace what is in the single quotes with the content for level 1
+var contentLevel1 = 'It is nice to meet adorable name. He participates fully in class. name can practice putting "a" in front of nouns. "I see a cat". name knows all of the letters and can match big to small. He can put together sentences with the help of the teacher. " I draw a line. I love Dino". He sang three songs today!! Please review the notes on the text box in the lesson video for specific words and grammar to review. Great 5 happy star job today.'
+
+//Replace what is in the single quotes with the content for level 2
+var contentLevel2 = 'It was very nice to see name today! He is progressing well in understanding English. He could say and read everything very well!. Excellent job. When working at home, name could underline the first letter of words in the workbook to help associate letters in addition to pictures with the words. This will help with reading. Please review the notes on the text box in the lesson video for specific words and grammar to review. Excellent 5 happy stars today! Keep working hard name!'
+
+//Replace what is in the single quotes with the content for level 3
+var contentLevel3 = 'It was very nice to see name today! name is a wonderful speaker! He knows all of the lesson vocabulary. He can deepen English language understanding with conversation which name did so well today. Great work with all of the grammar and vocabulary too! He can speak in full sentences. He answered lots of questions. name did a good job correcting errors when encouraged. Please review the notes on the text box in the lesson video for specific words and grammar to review. Overall a great job--5 happy stars today!'
+
+//Replace what is in the single quotes with the content for level 4
+var contentLevel4 = 'name is a wonderful reader. Wow - name is reading so fast. He also knows all of the vocabulary very well. He can deepen English language understanding with conversation which name did so well today. He answered lots of questions and used complete sentences in the answers. name could practice reading aloud between lessons to help with fluency. Please review the notes on the text box in the lesson video for specific words and grammar to review. So nice to see name today. 5 super stars!'
+
+//Replace what is in the single quotes with the content for level 5
+var contentLevel5 = 'Please review the notes on the text box in the lesson video for specific words and grammar to review.'
+
+//Replace what is in the single quotes with the content for level 6
+var contentLevel6 = 'Please review the notes on the text box in the lesson video for specific words and grammar to review.'
+
+/*
+var contentLevelUA1 = 'Content Level UA 1'
+
+var contentLevelUA2 = 'name is progressing so well in speaking! He is listening, paying attention, and repeating very well! name can circle first letters of words to help with reading. He understood all of the directions and circled all of the answers correctly too! Wow. 5 stars! He is creating originalsentences expressing ideas using correct English grammar.Keep working hard. Adorable student!! Please review the notes on the text box in the lesson video for specific words and grammar to review. Congratulations on an excellent assessment.'
+
+var contentLevelUA3 = 'Congratulations on a great assessment! I see progress in speaking and recognizing vocabulary words. 5 super stars! name can read aloud from the workbook between classes to help with fluency and pronunciation of longer words. Name ability to understand and remember what is read is excellent. Please review the notes on the text box in the lesson video for specific words and grammar to review. '
+
+var contentLevelUA4 = 'Content Level UA 1'
+
+var contentLevelUA5 = 'Content Level UA 1'
+
+var contentLevelUA6 = 'Content Level UA 1'
+
+var contentLevelUAPreVIP = 'Content Level UA 1'
+*/
+
+var openingSentence = "It was very nice to see name today. "
+
+var endingSentence = 'See you again soon! -From Teacher Deborah'
 
 var sampleSentencesL1 = ['boy is progressing so well in speaking.', 'boy is listening, paying attention, and repeating very well.', 
 'boy can circle first letters of words to help with reading.', 'boy understood all of the directions and circled all of the answers correctly too.', 
@@ -23,9 +73,49 @@ var sampleSentencesL4 = ['5 happy stars today! ', 'boy is progressing well in un
 var sampleSentencesTrial = ['It was so nice to meet you today.', 'boy is a hardworking student.', 'boy could repeat all of the words and sentences.', 'Excellent 5 star job.', 
 'I think boy could continue to rapidly advance in English with VIPKID.', 'I think starting at a young age gives a big advantage.', 'boy is an excellent candidate for VIPKID.']
 
+var sampleSentences = ['He is a hard working student.', 'He speaks very well!', 'He could repeat all of the words and letters.', 'He is young and learning this type of classroom.', 'I am impressed at how quickly name learns!', 'He will feel comfortable to learn English.', 'Excellent 5 star job!', 'I think with practice in this type of setting, name will rapidly advance in the program.', 'I think starting at a young age gives name a big advantage.', 'He is progressing so well in speaking.', 'name is listening, paying attention, and repeating very well.', 
+'Keep working hard.', 'Great job this lesson.', 'He is progressing well in understanding.', 'name can follow the directions easily.', 'He is wonderful and hardworking and I see progress throughout the class.', 'He is an exceptional student.', 'I can see that name is working very hard on his English.', 'Please review the notes on the text box in the lesson video for specific words and grammar to review.', 'He participates fully in class.']
 
-var endingSentences = ['See you next time from teacher Deborah.', 'See you soon from teacher Deborah.']
 
+function generateReview(gender, name, content)
+{
+	content = content.replace(/name/g, name)
+	if (gender == 'She')
+	{
+		content = content.replace(/He/g, 'She')
+	}
+	var review = content + endingSentences
+	var premadesDiv = document.getElementById('premades')
+	var reviewString = "<div class = 'review'><textarea rows='7' cols='90'>" + review + "</textarea></div>"
+	premadesDiv.innerHTML += reviewString
+}
+
+function randomReview(gender, name)
+{
+	var review = openingSentence //first sentence
+	var usedIndexes = []
+	//random sentences from bank until 50 words
+	while (review.split(' ').length < 50)
+	{
+		var randomIndex = Math.floor(Math.random() * sampleSentences.length);
+		if (!usedIndexes.includes(randomIndex))
+		{
+			usedIndexes.push(randomIndex)
+			var randomSentence = sampleSentences[randomIndex];
+			review += randomSentence;
+			review += ' '
+		}
+	}
+	review += endingSentence // ending sentence
+	review = review.replace(/name/g, name)
+	if (gender == 'She')
+	{
+		review = review.replace(/He/g, 'She')
+	}
+	var premadesDiv = document.getElementById('premades')
+	var reviewString = "<div class = 'review'><textarea rows='7' cols='90'>" + review + review.split(' ').length + "</textarea></div>"
+	premadesDiv.innerHTML += reviewString
+}
 
 function createRandomizedReview(gender, name, sampleSentences)
 {
@@ -62,6 +152,8 @@ function createRandomizedReview(gender, name, sampleSentences)
 }
 
 
+
+
 function viewReview(review)
 {
 	var premadesDiv = document.getElementById('premades')
@@ -69,7 +161,7 @@ function viewReview(review)
 	premadesDiv.innerHTML += reviewString
 }
 
-function generate()
+function display()
 {
 	var form = document.getElementById("formL1");
 	console.log(form.gender)
@@ -78,17 +170,58 @@ function generate()
 	switch (form.level.value)
 	{
 		case '1':
-			createRandomizedReview(form.gender.value, form.name.value, sampleSentencesL1);
+			generateReview(form.gender.value, form.name.value, contentLevel1);
 			break;
 		case '2':
-			createRandomizedReview(form.gender.value, form.name.value, sampleSentencesL2);
+			generateReview(form.gender.value, form.name.value, contentLevel2);
 			break;
 		case '3':
-			createRandomizedReview(form.gender.value, form.name.value, sampleSentencesL3);
+			generateReview(form.gender.value, form.name.value, contentLevel3);
 			break;
-		case 'trial':
-			createRandomizedReview(form.gender.value, form.name.value, sampleSentencesTrial);
+		case '4':
+			generateReview(form.gender.value, form.name.value, contentLevel4);
 			break;
+		case '5':
+			generateReview(form.gender.value, form.name.value, contentLevel5);
+			break;
+		case '6':
+			generateReview(form.gender.value, form.name.value, contentLevel6);
+			break;
+		case '7':
+			generateReview(form.gender.value, form.name.value, contentPreVIPTrial);
+			break;
+		case '8':
+			generateReview(form.gender.value, form.name.value, contentNonreaderTrial);
+			break;
+		case '9':
+			generateReview(form.gender.value, form.name.value, contentReaderTrial);
+			break;
+		case '10':
+			generateReview(form.gender.value, form.name.value, contentMCPreVIP);
+			break;
+		/*
+		case '11':
+			generateReview(form.gender.value, form.name.value, contentLevelUAPreVIP);
+			break;
+		case '12':
+			generateReview(form.gender.value, form.name.value, contentLevelUA1);
+			break;
+		case '13':
+			generateReview(form.gender.value, form.name.value, contentLevelUA2);
+			break;
+		case '14':
+			generateReview(form.gender.value, form.name.value, contentLevelUA3);
+			break;
+		case '15':
+			generateReview(form.gender.value, form.name.value, contentLevelUA4);
+			break;
+		case '16':
+			generateReview(form.gender.value, form.name.value, contentLevelUA5);
+			break;
+		case '17':
+			generateReview(form.gender.value, form.name.value, contentLevelUA6);
+			break;
+		*/
 		default:
 			//createRandomizedReview(form.gender.value, form.name.value, endingSentences);
 			break;
@@ -96,6 +229,17 @@ function generate()
 
 	console.log(sampleSentences)
 }
+
+function generate()
+{
+	var form = document.getElementById("formL1");
+	console.log(form.gender)
+	var sampleSentences = ''
+	console.log(form.level.value)
+	randomReview(form.gender.value, form.name.value)
+
+}
+
 
 
 document.addEventListener('DOMContentLoaded', function()
